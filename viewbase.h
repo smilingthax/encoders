c2ff6bf4fe87c327a4f45e9b6ddc256549dba79f
Tobias Hoffmann <thobi@worker>
2013-03-16 05:34:10 +0100

    forgotten rename in C++03 section

--- viewbase.h
+++ viewbase.h
@@ -142,7 +142,7 @@ private:
   template <int N,typename T0,typename T1,typename T2,typename T3,
                   typename T4,typename T5,typename T6,typename T7>
   bool get(T0 &t0,T1 &t1,T2 &t2,T3 &t3,T4 &t4,T5 &t5,T6 &t6,T7 &t7) {
-    return do_nonnil_and_get_safe(t0,t1,t2,t3,t4,t5,t6,t7);
+    return do_nonnil_and_get_one(t0,t1,t2,t3,t4,t5,t6,t7);
   }
   template <int N,typename T0,typename T1,typename T2,typename T3,
                   typename T4,typename T5,typename T6,typename T7>
@@ -153,7 +153,7 @@ private:
         return false;
       }
     }
-    return do_nonnil_and_put_safe(t0,t1,t2,t3,t4,t5,t6,t7);
+    return do_nonnil_and_put_one(t0,t1,t2,t3,t4,t5,t6,t7);
   }
 private:
 #define DO_NONNIL_AND(fun) \
